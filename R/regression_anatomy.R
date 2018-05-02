415e1857880eba1b9e3dca601682554557f9177f
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2018-05-02 15:35:29 +0200

    First commit

---
+++ R/regression_anatomy.R
@@ -0,0 +1,291 @@
+example.regression.anatomy = function() {
+  setwd("D:/libraries/rapa")
+  dat = foreign::read.dta("intdata.dta")
+
+  library(dplyr)
+  dat = mutate(dat, log_top1 = log(top1), log_mtr = log(mtr), Year = as.factor(year))# %>% filter(year == 2005)
+  reg1 = lm(log_top1 ~ log_mtr, data=dat)
+  reg2 = lm(log_top1 ~ log_mtr + gdpcap, data=dat)
+
+  yvar = "log_top1"
+  cvar = c("log_mtr","Year")
+  xvar = "gdpcap"
+
+  dat = na.omit(dat)
+
+  dat$org_y = dat[[yvar]]
+  df = add.tilde.xy(dat,yvar, xvar, cvar)
+
+
+
+
+  ggplot(data=df, aes_string(x=xvar, y=yvar, color="country")) + geom_point() + geom_smooth(method="lm", color="black") + facet_wrap(~.control)
+
+
+  ggplot(data=df, aes_string(x=xvar, y=yvar, color="org_y")) + geom_point() + geom_smooth(method="lm", color="black") + facet_wrap(~.control) + scale_colour_gradient()
+
+  plot()
+
+  yreg = lm(log_top1 ~ gdpcap)
+
+  dat$y.tilde = resid()
+
+  gg = ggplot()
+
+
+
+}
+
+simpsons.example = function() {
+  dat = mosaicData::SAT
+
+  # Create state abbreviations
+  setNames(state.abb, state.name)
+  dat$code = state.abb[dat$state]
+
+
+  xvar = "sat"
+  cvar = c("frac")
+  yvar = "salary"
+
+  dat = na.omit(dat)
+
+  dat$org_x = dat[[xvar]]
+
+  df = add.tilde.xy(dat,yvar, xvar, cvar, adjust.xy="x")
+
+
+  ggplot(data=df, aes_string(x=xvar, y=yvar, color=".dist.x", label="code")) + geom_text() + geom_smooth(method="lm") + facet_wrap(~.control) + scale_colour_gradient2(low="#ff0000", high="#0000ff", mid="#000000", midpoint = 0) + theme_bw()
+
+  d = filter(df, !(.adjust=="xy" & .control.ind==0))
+
+  p = ggplotly(
+    ggplot(data=d, aes_string(x=xvar, y=yvar, color="org_x", label="code", frame=".frame")) + geom_text() + geom_smooth(method="lm", se=FALSE) + scale_colour_gradient2(low="#ff0000", high="#0000ff", mid="#000000", midpoint = mean(range(df$org_x))) + theme_bw()
+
+  ) %>% config(displayModeBar = F) %>% animation_opts(frame = 3000, transition = 3000, easing = "linear", redraw = FALSE, mode = "immediate") %>% animation_button()
+
+  p = p %>% animation_slider(currentvalue = list(prefix = "", font = list(color="white")))
+  p
+
+  devtools::install_github('hadley/ggplot2', dep=FALSE)
+}
+
+
+
+add.tilde.xy = function(dat, yvar, xvar, cvars,frames=NULL,  org.x.col = ".org.x", dist.x.col=".dist.x", frame.col=".frame", frame.ind.col=".frame.ind", control.ind.col = ".control.ind", control.col = ".control", adjust.col = ".adjust", control.label = paste0("control ", seq_along(cvars), ": ", cvars), no.control.label = "no control", adjust.control = c("sim","seq")[1], adjust.xy=c("seq","sim","x")[1], duplicate.org=FALSE) {
+  restore.point("add.tilde.xy")
+
+  if (adjust.xy == "x") yvar = NULL
+  if (is.null(yvar)) adjust.xy = "x"
+
+
+  if (is.null(frames)) {
+    frames = "original"
+    if (duplicate.org) frames = c(frames,"original2")
+
+    if (adjust.control=="sim") {
+      if (adjust.xy == "seq") {
+        frames = c(frames,"control x","control y")
+      } else if (adjust.xy == "sim") {
+        frames = c(frames,"control")
+      } else if (adjust.xy == "x") {
+        frames = c(frames, "control x")
+      } else {
+        stop("Unknown adjust.xy argument.")
+      }
+    } else if (adjust.control=="seq") {
+      if (adjust.xy == "seq") {
+        grid = expand.grid(c("x","y"), cvars)
+        frames = c(frames,paste0("control ", grid[,1], " ", grid[,2]))
+      } else if (adjust.xy == "sim") {
+        frames = c(frames,paste0("control ", cvars))
+      } else if (adjust.xy == "x") {
+        frames = c(frames,paste0("control x", cvars))
+      } else {
+        stop("Unknown adjust.xy argument.")
+      }
+    } else {
+      stop("Unknown adjust.control argument.")
+    }
+    frames = paste0(seq_along(frames), ". ", frames)
+  }
+
+
+  li = vector("list", length(frames))
+
+  i = 1
+  dat[[frame.col]] = frames[i]
+  dat[[frame.ind.col]] = i
+  dat[[control.col]] = no.control.label
+  dat[[control.ind.col]] = 0
+  dat[[org.x.col]] = dat[[xvar]]
+  dat[[dist.x.col]] = 0
+  if (adjust.xy == "sim") {
+    dat[[adjust.col]] = "xy"
+  } else {
+    dat[[adjust.col]] = "x"
+  }
+  li[[i]] = dat
+
+  if (adjust.xy == "seq" & duplicate.org) {
+    i = i +1
+    dat[[adjust.col]] = "xy"
+    dat[[frame.col]] = frames[i]
+    dat[[frame.ind.col]] = i
+    li[[i]] = dat
+  }
+
+
+  if (adjust.control == "seq") {
+    for (cvar.ind in seq_along(cvars)) {
+      cvar = cvars[cvar.ind]
+      d = dat
+
+      dat[[xvar]] = anatomy.tilde.value(dat[[xvar]],dat[[cvar]])
+      dat[[control.col]] = control.label[cvar.ind]
+      dat[[control.ind.col]] = cvar.ind
+      dat[[dist.x.col]] = dat[[xvar]] - dat[[org.x.col]]
+
+      if (adjust.xy == "sim") {
+        dat[[yvar]] = anatomy.tilde.value(dat[[yvar]],dat[[cvar]])
+        dat[[adjust.col]] = "xy"
+      } else {
+        dat[[adjust.col]] = "x"
+      }
+      i = i +1
+      dat[[frame.col]] = frames[i]
+      dat[[frame.ind.col]] = i
+      li[[i]] = dat
+
+      if (adjust.xy == "seq") {
+        dat[[yvar]] = anatomy.tilde.value(dat[[yvar]],dat[[cvar]])
+        i = i +1
+        dat[[frame.col]] = frames[i]
+        dat[[frame.ind.col]] = i
+        dat[[adjust.col]] = "xy"
+        li[[i]] = dat
+      }
+    }
+  # Add simultaneously all controls
+  } else {
+    d = dat
+
+    dat[[xvar]] = anatomy.tilde.value(dat[[xvar]],dat[cvar])
+    dat[[control.col]] = "control"
+    dat[[control.ind.col]] = 1
+    dat[[dist.x.col]] = dat[[xvar]] - dat[[org.x.col]]
+
+    if (adjust.xy == "sim") {
+      dat[[yvar]] = anatomy.tilde.value(dat[[yvar]],dat[cvar])
+      dat[[adjust.col]] = "xy"
+    } else {
+      dat[[adjust.col]] = "x"
+    }
+    i = i +1
+    dat[[frame.col]] = frames[i]
+    dat[[frame.ind.col]] = i
+
+    li[[i]] = dat
+
+    if (adjust.xy == "seq") {
+      dat[[yvar]] = anatomy.tilde.value(dat[[yvar]],dat[cvar])
+      i = i +1
+      dat[[frame.col]] = frames[i]
+      dat[[frame.ind.col]] = i
+      dat[[adjust.col]] = "xy"
+      li[[i]] = dat
+    }
+  }
+  df = do.call(rbind, li)
+
+  levels = c(no.control.label,control.label)
+  df[[control.col]] = ordered(df[[control.col]], levels)
+
+  df[[frame.col]] = ordered(df[[frame.col]], frames)
+
+
+  df
+
+}
+
+
+anatomy.tilde.value = function(val, control, add.mean = TRUE) {
+  if (is.data.frame(control)) {
+    has.factor = any(sapply(control, function(x) is.factor(x) | is.character(x)))
+    if (!has.factor) {
+      control = as.matrix(control)
+    }
+  } else {
+    has.factor = is.factor(control) | is.character(control)
+    if (has.factor) {
+      control = data.frame(var=control)
+    }
+  }
+
+  if (is.data.frame(control)) {
+    formula = as.formula(paste0("val ~ ", paste0("control$", colnames(control), collapse=" + ")))
+    tilde = resid(lm(formula))
+  } else {
+    tilde = resid(lm.fit(y = val,x = cbind(1,control)))
+  }
+
+  if (add.mean) tilde = tilde + mean(val)
+  return(tilde)
+}
+
+regression.anatomy.x.tilde = function(mod,X=model.matrix(mod), add.mean = TRUE) {
+  xdat = as.matrix(X)
+  if (all(xdat[,1]==1)) xdat=xdat[,-1]
+
+  x.tilde = xdat
+
+  #dat = get.regression.data(mod)
+  #xdat = as.matrix(dat[,-1])
+  xvars = colnames(xdat)
+
+  i = 1
+  for (i in seq_along(xvars)) {
+    x.tilde[,i] = resid(lm.fit(y = xdat[,i],x = cbind(1,xdat[,-i]))) + ifelse(add.mean,mean(xdat[,i]))
+  }
+  as.data.frame(x.tilde)
+}
+
+
+#' Regression anatomy plots
+#'
+#' Show original scatterplots and scatterplots
+#' of y and controlled x_i, i.e. the residuals
+#' of a regression of x_i on the other explanatory
+#' variables. See e.g. Filoso (2010):
+#' https://mpra.ub.uni-muenchen.de/42716/1/FILOSO-Regression_Anatomy_Revealed.pdf
+
+#' @param mod A regression model, a result of a call to lm.
+#' @param vars which x variables shall be included in the plot
+#' @export
+regression.anatomy.plots = function(mod, vars=NULL) {
+  dat = get.regression.data(mod)
+  xvars = colnames(dat)[-1]
+  x.tilde = regression.anatomy.x.tilde(mod)
+  tilde = cbind(dat[[1]],x.tilde)
+  colnames(tilde) = colnames(dat)
+
+  dat$TRANSFORM = "orginal"
+  tilde$TRANSFORM = "controlled"
+
+  yvar = colnames(dat)[1]
+  d = rbind(dat, tilde)
+  library(tidyr)
+
+  dl = gather_(d,key_col="xvar",value_col="x", gather_cols=xvars)
+
+  if (!is.null(vars)) {
+    dl = filter(dl, xvar %in% vars)
+  }
+
+  library(ggplot2)
+  ggplot(data=dl, aes_string(y=yvar,x="x",color="xvar")) +
+    geom_point()+
+    geom_smooth(method = lm,se = FALSE, color="black")+
+    facet_grid(TRANSFORM~xvar,scales = "free_x")
+
+}
---
+++ R/regtools.R
@@ -0,0 +1,196 @@
+examples.expand.formula.and.data = function() {
+  T = 10
+  x1 = sample(1:4, T, replace = TRUE)
+  x2 = runif(T)
+
+  y = x1^2 + log(x2) + rnorm(T)
+  x1f = factor(x1)
+
+  formula = y~x1f+log(x2) + x1*x2 + I(x1^2)
+  terms(formula)$variables[[1]]
+  formula.dep.var(formula)
+  formula.indep.var(formula)
+
+  dat = regression.data(formula)
+  regression.formula(dat)
+}
+
+# Returns the dependent variable of a formula object
+# @export
+formula.dep.var = function(formula) {
+  all.vars(formula[[2]])
+}
+
+# Returns the name of the dependent variable of a model object
+# @export
+model.dep.var = function(mod) {
+  if (is(mod,"felm")) return(mod$lhs)
+  all.vars(formula(mod)[[2]])
+}
+
+# Creates a data.frame that can be used to run a regression with the given formula
+#
+# @param formula a regression formula e.g. y~log(x1)+x2
+# @param reg optionally a regression object from which the formula is takes
+# @param data a data frame that contains original variables used in formula
+# @param normalize.names if TRUE (default) change column names to be valid R variable names
+# @param remove.intercept if TRUE (default) do not generate an .Intercept column
+# @param expand if TRUE, call expanded.regression.data instead
+#
+# @return a data.frame
+# @export
+regression.data = function(formula=stats::formula(reg), reg=NULL, data=NULL, normalize.names=TRUE, remove.intercept=TRUE, expand=FALSE) {
+  restore.point("regression.data")
+  if (is.null(reg) & is.null(data))
+    return(NULL)
+
+  if (!is.null(reg)) {
+    dv = model.dep.var(reg)
+  } else {
+    dv = formula.dep.var(formula)
+  }
+  dep.var = data[[dv]]
+  #browser()
+  # remove NA from depvar
+  rownames(data) = seq_len(NROW(data))
+  if (!expand) {
+    mf = model.frame(formula,data=data)
+  } else {
+    # all factors are expanded
+    mf = as.data.frame(model.matrix(formula,data))
+    if (remove.intercept) {
+      mf = mf[,-1]
+    }
+  }
+  if (NROW(mf) != NROW(dep.var)) {
+    rows = as.integer(rownames(mf))
+    dep.var = dep.var[rows]
+  }
+
+  dat = cbind(dep.var,mf)
+  if (normalize.names) {
+    names = normalize.column.names(colnames(dat))
+    colnames(dat)=names
+  }
+  dat
+}
+
+
+# Creates a data.frame with all dummy variables and transformed variables described in formula
+#
+# @param formula a regression formula e.g. y~log(x1)+x2
+# @param data a data frame that contains original variables used in formula
+# @param normalize.names if TRUE (default) change column names to be valid R variable names
+# @param remove.intercept if TRUE (default) do not generate an .Intercept column
+#
+# @return a data.frame
+# @export
+expanded.regression.data = function(formula, data=parent.frame(), normalize.names=TRUE, remove.intercept=TRUE,...) {
+  regression.data(formula=formula,data=data,normalize.names=normalize.names, remove.intercept = remove.intercept, expand=TRUE,...)
+}
+
+# generates a regression formula from a data.frame assuming the first column is regressed on all other columns
+# @export
+regression.formula = function(data, lhs=colnames(data)[1], rhs=setdiff(colnames(data),lhs)) {
+  str = paste0(lhs,"~",paste0(rhs,collapse="+"))
+  as.formula(str)
+}
+
+
+# Normalize column names such that they are valid variable names in R
+# @export
+normalize.column.names <- function(names) {
+  names = gsub("-","_",names,fixed=TRUE)
+  names = gsub(" ","_",names,fixed=TRUE)
+  names = gsub("(","_",names,fixed=TRUE)
+  names = gsub(":",".",names,fixed=TRUE)
+  names = gsub("*:",".",names,fixed=TRUE)
+  names = gsub(")","",names,fixed=TRUE)
+  names = gsub("^","_power_",names,fixed=TRUE)
+  names
+}
+
+# Rerun the regression reg with a formula and data in which all factors and transformations are expanded to separate (dummy-)variables.
+# @param reg the original regression object (needs to have a fields call, formula and data)
+# @export
+expanded.regression = function(reg, org.formula=formula(reg), org.data=get.regression.data(reg), parent.env = parent.frame()) {
+  restore.point("expanded.regression")
+  substitute.call = function (x, env) {
+    call <- substitute(substitute(x, env), list(x = x))
+    eval(call)
+  }
+
+  env = new.env(parent=parent.env)
+  edat = expanded.regression.data(org.formula,org.data)
+  colnames(edat) = normalize.column.names(colnames(edat))
+  eformula = regression.formula(edat)
+
+  call = reg$call
+  call$formula = eformula
+  call$data = as.name("edat")
+
+  env$edat = edat
+  eval(call,env)
+}
+
+# Try to extract name of the dependent variable from a regression model
+# @export
+name.of.depvar = function(reg) {
+  if (is(reg,"felm")) return(reg$lhs)
+  names(reg$model)[[1]]
+}
+
+# Extract data from a regression model
+#
+# First looks whether the model mod has a field 'custom.data' in which a user can manually store the data.frame.
+#
+get.regression.data = function(mod, source.data=NULL,...) {
+  restore.point("get.regression.data")
+
+  if (!is.null(source.data)) {
+    res = regression.data(reg=mod,data = source.data,normalize.names = FALSE)
+    return(res)
+  }
+
+  if (is(mod,"felm")) {
+    dat = as.data.frame(cbind(mod$response, mod$X))
+    return(dat)
+  }
+
+  if (!is.null(mod[["custom.data"]]))
+    return(mod$custom.data)
+
+  if (!is.null(mod[["data"]]))
+    return(mod$data)
+
+
+  # Model frame
+  if (!is.null(mod[["model"]]))
+    return(mod[["model"]])
+
+  warning("Could not retrieve regression data.")
+  return(NULL)
+}
+
+# Extract values of the variable var from a regression model mod
+#
+# Reports an error if the data for that variable cannot get extracted
+# from the regression model mod.
+#
+# @export
+get.regression.var = function(mod, var, as.data.frame = length(var)>1) {
+  restore.point("get.regression.var")
+
+  data = get.regression.data(mod)
+
+  has.var = var %in% names(data)
+  if (!all(has.var)) {
+    missing.var = paste0(var[!has.var], collapse=", ")
+    stop("I could not find the variable(s) ", missing.var, " in your regression model. Try to add a data frame with that variable as field 'custom.data' to your model object.")
+  }
+  if (as.data.frame | length(var)>1)
+    return(as.data.frame(data[var]))
+
+  data[[var]]
+}
+
